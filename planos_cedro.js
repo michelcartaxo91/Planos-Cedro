261cd4c51d0e9bcbb44872d5a2cc7759b48abb26
Michel Cartaxo <37109836+michelcartaxo91@users.noreply.github.com>
2018-04-12 14:48:32 -0300

    Add files via upload

---
+++ planos_cedro.js
@@ -0,0 +1,17 @@
+function isChecked()
+{
+	if (document.getElementById("LiCheck").checked)
+		return true;
+	else
+		return false;	
+}
+
+function cor(champs) 
+{
+	var show = document.getElementById("planos");
+
+	$(".planos").removeClass('active');
+	champs.classList.add("active");
+	$(champs).find(".check").prop('checked', true);
+	
+} 
